fc4ed7a606a949d732db6bfcf053f5281a1ef2ed
Roger Chuang <roger.chuang@ileopard.com>
2019-03-06 22:57:25 +0800

    Migrate to AndroidX

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+object Versions {
+    // Build
+    val android_build_tool = "3.3.2"
+
+    // Kotlin
+    val kotlin = "1.3.20"
+    val coroutine = "1.1.1"
+
+    // Android
+    val app_compat = "1.1.0-alpha02"
+    val constraint_layout = "1.1.3"
+
+    // Test
+    val junit = "4.12"
+    val android_test_runner = "1.1.2-alpha01"
+    val espresso = "3.1.2-alpha01"
+}
